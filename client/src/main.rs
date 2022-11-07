ee2082fd663dda4255d9c9d1d81e6ece01b47bb1
Ruchira Sachinthana <rucsacman@gmail.com>
2022-11-07 10:50:42 +0530

    Fixed the issues in the broadcasting Refactor the code

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,45 +1,38 @@
-
 use std::{
-    thread,
     io::{self, Read, Write},
     net::TcpStream,
+    thread,
     time::Duration,
 };
 
 fn main() {
+    let mut stream = TcpStream::connect("127.0.0.1:7878").expect("Connection faild");
+    let mut input_stream = stream.try_clone().unwrap();
+    let duration = Duration::from_millis(100);
 
-	let mut stream = TcpStream::connect("127.0.0.1:7878").expect("Connection faild");
-	let mut input_stream = stream.try_clone().unwrap();
-    let duration = Duration::from_millis(50);
-    
-	thread::spawn(move || {
+    thread::spawn(move || {
         let mut client_buffer = [0u8; 1024];
-        // input_stream.set_read_timeout(Some(duration));
 
-		loop {
-			match input_stream.read(&mut client_buffer) {
-				Ok(n) => {
-
-						io::stdout().write(&client_buffer).unwrap();
-						io::stdout().flush().unwrap();
-                        thread::sleep(duration);
-				},
-				Err(error) => {
+        loop {
+            match input_stream.read(&mut client_buffer) {
+                Ok(_n) => {
+                    io::stdout().write(&client_buffer).unwrap();
+                    io::stdout().flush().unwrap();
+                }
+                Err(error) => {
                     println!("Connection Failed {}", error);
                     break;
-                },
-			}
-            TcpStream::connect_timeout(&"127.0.0.1:7878".parse().unwrap(), duration).unwrap();
-		}
-	});
+                }
+            }
+            thread::sleep(duration);
+        }
+    });
 
-    let output_stream = &mut stream;
     let mut user_buffer = String::new();
 
     loop {
         io::stdin().read_line(&mut user_buffer).unwrap();
-
-        output_stream.write(user_buffer.as_bytes()).unwrap();
-        output_stream.flush().unwrap();
+        stream.write(user_buffer.as_bytes()).unwrap();
+        stream.flush().unwrap();
     }
 }
--- std_server/src/main.rs
+++ std_server/src/main.rs
@@ -39,50 +39,52 @@ async fn handle_connection(
         println!("size: {}", boradcast_map.len());
     }
 
-    let mut index = 0;
-
     loop {
         let mut buffer = [1; 500];
-        let mut len = 0;
+        let mut _len = 0;
 
         match timeout(Duration::from_millis(50), stream.read(&mut buffer)).await {
             Ok(result) => {
                 let result = match result {
                     Ok(bytes) => {
                         // println!("received bytes {}", bytes);
-                        len = bytes
+                        _len = bytes
                     }
                     Err(e) => {
                         println!("unable to read the data from the stream error: {}", e);
-                        len = 0;
+                        _len = 0;
                         return;
                     }
                 };
                 result
             }
             Err(_) => {
-                len = 0;
+                _len = 0;
             }
         };
 
         let mut boradcast_vec = VecDeque::new();
 
-        if len > 3 {
-            {
-                let client_map = &mut *client_lock.write().unwrap();
-                let message = String::from_utf8_lossy(&mut buffer[..len]);
+        {
+            let client_map = &mut *client_lock.write().unwrap();
+            if _len > 3 {
+                let message = String::from_utf8_lossy(&mut buffer[.._len]);
 
                 for (key, message_deque) in &mut *client_map {
-                    println!("clinet: {} | Push back :  key: {}  value : {} ", client_id, key, message);
+                    println!(
+                        "clinet: {} | Push back :  key: {}  value : {} ",
+                        client_id, key, message
+                    );
                     message_deque.push_back(message.to_string());
                 }
-                boradcast_vec = client_map.get_mut(&client_id).unwrap().clone();
             }
+            boradcast_vec = client_map.get_mut(&client_id).unwrap().clone();
+            client_map.get_mut(&client_id).unwrap().clear();
         }
 
         let mut borrow_vec = boradcast_vec.borrow_mut();
         let mut message: String = String::new();
-       
+
         loop {
             if borrow_vec.len() == 0 {
                 break;
@@ -90,33 +92,30 @@ async fn handle_connection(
 
             let value = borrow_vec.pop_front().unwrap();
             message.push_str(&value);
+        }
 
-            match timeout(Duration::from_millis(50), stream.write(value.as_bytes())).await {
+        if message.len() > 0 {
+            println!("clinet: {}  write message : {} ", client_id, message);
+
+            match timeout(Duration::from_millis(50), stream.write(message.as_bytes())).await {
                 Ok(result) => {
                     let result = match result {
                         Ok(bytes) => {
                             let _ = stream.flush().await.unwrap();
-                            len = bytes  
+                            _len = bytes
                         }
                         Err(e) => {
                             println!("unable to write the data from the stream error: {}", e);
-                            len = 0;
+                            _len = 0;
                         }
                     };
                     result
                 }
                 Err(_) => {
-                    len = 0;
+                    _len = 0;
                 }
             };
         }
-        {
-            let client_map = &mut *client_lock.write().unwrap();
-            client_map.get_mut(&client_id).unwrap().clear();
-            message.clear();
-            borrow_vec.clear()
-        }
-
     }
 }
 
